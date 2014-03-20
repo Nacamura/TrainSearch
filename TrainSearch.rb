6c9e83e36494c3f44b4e924be09cb7f697da51d4
Nacamura <t.nacamura@gmail.com>
2014-03-20 23:12:28 +0900

    Separate JSON

--- TrainSearch.rb
+++ TrainSearch.rb
@@ -1,14 +1,13 @@
-require 'json'
+load './MyJSON.rb'
 load './MyLogger.rb'
 load './TwitCommunicator.rb'
 load './JorudanSearch.rb'
 
 class TrainSearch
   include MyLogger
-  attr_accessor :routes
 
   def execute
-    settings = load_json("settings.txt")
+    settings = MyJSON.load_json("settings.txt")
     twitcom = TwitCommunicator.new(settings)
     tweets = twitcom.gather_new_tweets
     tweets.each do |t|
@@ -21,18 +20,6 @@ class TrainSearch
       if(lines[1].strip == "home")
         twitcom.route_home
       end
-    end
-  end
-
-  def load_json(textfile_path)
-    open(textfile_path) do |io|
-      JSON.load(io)
-    end
-  end
-
-  def store_json(array, textfile_path)
-    open(textfile_path, "w") do |io|
-      JSON.dump(array, io)
     end
   end
 
